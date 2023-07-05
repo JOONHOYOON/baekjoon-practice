b7cbd0f04a9af9e6a747bf43a6deb03ebbf827fd
joonho <joonho9892@gmail.com>
2023-07-05 10:32:38 +0900

    baekjoon 1단계 완료

--- src/problem/Main.java
+++ src/problem/Main.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		String id=sc.next();
-		System.out.println(id+"??!");
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
 	}
 
 }
